f8874354cf070b7c8f059e1f8fe28cac1da26617
Marek Płonka <marek.plonka@nask.pl>
2018-01-03 10:32:45 +0100

    modified:   chromium/scripts/chromium.json 	modified:   chromium/scripts/run.sh

--- chromium/scripts/run.sh
+++ chromium/scripts/run.sh
@@ -1,4 +1,5 @@
 #!/bin/bash - 
 export LANG=pl_PL.UTF-8
-#/usr/bin/chromium --window-size="1440,900"
+# /usr/bin/chromium --window-size="1440,900"
 /usr/bin/chromium --window-size="1440,900" https://www.pekao24.pl
+# exec /usr/bin/vim
