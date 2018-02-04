57803f3fe86602eebe7b49b19019d0d39ccf4c5f
marekpl <marekpl@nask.pl>
2018-02-04 18:24:38 +0100

    2018-02-04 Sun 18:24:39 PM

---
+++ eg+/scripts/run.sh
@@ -0,0 +1,26 @@
+#!/bin/bash - 
+#===============================================================================
+#
+#          FILE: run.sh
+# 
+#         USAGE: ./run.sh 
+# 
+#   DESCRIPTION: 
+# 
+#       OPTIONS: ---
+#  REQUIREMENTS: ---
+#          BUGS: ---
+#         NOTES: ---
+#        AUTHOR: Marek Płonka (marekpl), marek.plonka@nask.pl
+#  ORGANIZATION: NASK
+#       CREATED: 31.10.2016 12:26:01
+#      REVISION:  ---
+#===============================================================================
+
+#set -o nounset                              # Treat unset variables as an error
+
+/etc/init.d/ssh start
+/etc/init.d/nginx start
+#exec supervisord -n
+bash -c 'sleep infinity'
+
